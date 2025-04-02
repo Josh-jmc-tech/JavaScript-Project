cf3641ed73f83dce7189f97b8ed8c0b96c7363a6
Joshua Baldon <baldonvaljoshua97@gmail.com>
2025-04-02 18:07:59 -0400

    adding JS project to remote repo

---
+++ Basic JavaScript Project/Project1_expression_alert/JS/main.js
@@ -0,0 +1,12 @@
+var part1 = "Lisa told Bart, \"Knock it off, Bart! Or I'll tell Dad!\"<br>\"Eat my shorts!\" Bart responded."; // using aposthrope on var's
+
+var part2 = "<br><br>\"Don\'t be sad, be happy,"+"Because life is short"+"<br>-Josh"; // concatenated string
+var concatenatedString = part1 + part2;
+
+document.write(concatenatedString)
+
+var Philippines = "Manila", Canada = "Toronto", Russia = "Moscow", Italy = "Palermo"; // multiple variables
+document.write(Russia)
+
+
+document.write(3 + 7); // using expression
