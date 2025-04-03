881950fd19d5842c703632f1c091bb5692f2299f
Joshua Baldon <baldonvaljoshua97@gmail.com>
2025-04-02 20:01:51 -0400

    Project2_functions file

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function LeFunction() {
+    var str = "Hello, I'm SiSu";
+    document.getElementById("Button_Text").innerHTML = str
+}
+
+function myFunction() {
+    var sentence = "I love reading books";
+    sentence += " and History";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
